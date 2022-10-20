42e9be36ae5e87d0d00779977f6deee78eb802fd
catapart <75452248+catapart@users.noreply.github.com>
2022-10-20 15:11:53 -0500

    changed key to be a nullable property;

--- SpecificationItem.cs
+++ SpecificationItem.cs
@@ -5,7 +5,7 @@ namespace Magnit.Tokenization
     public sealed class SpecificationItem
     {
         public Regex Regex { get; set; }
-        public string Key { get; set; }
+        public string? Key { get; set; }
         public string[]? CaptureGroups { get; set; }
         public string? GroupJoinValue { get; set; }
         public Func<string, Task<string>>? ResultCallback { get; set; }
